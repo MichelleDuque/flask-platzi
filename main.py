ecccbe08cfcea92f6c1b8c1883e1e7e3250fae88
MichelleDuque <michelled_1994@hotmail.com>
2022-06-08 18:28:17 -0500

    Se agrego log out

--- main.py
+++ main.py
@@ -4,16 +4,16 @@
 # $env:FLASK_ENV="development"
 # flask run
 
-from flask import request, make_response, redirect, render_template, session, url_for, flash
 import unittest
+from flask import request, make_response, redirect, render_template, session, url_for, flash
+from flask_login import login_required, current_user
+
 
 from app import create_app
 from app.forms import LoginForm
+from app.firestore_service import get_users, get_todos
 
 app = create_app()
-
-
-todos = ['Comprar cafe', 'Enviar solicitud de compra', 'Entregar video a productor ']
 
 
 @app.cli.command()
@@ -43,13 +43,14 @@ def index():
 
 
 @app.route('/hello', methods=['GET'])
+@login_required
 def hello():
     user_ip = session.get('user_ip')
-    username = session.get('username')
+    username = current_user.id
 
     context = {
         'user_ip': user_ip,
-        'todos': todos,
+        'todos': get_todos(user_id=username),
         'username': username
     }
 
