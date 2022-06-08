5f49727151ff1aaaebd85f0d7109ff1d37796dad
MichelleDuque <michelled_1994@hotmail.com>
2022-06-08 17:06:34 -0500

    Dirigir el login al home por medio de blueprints

--- tests/test_base.py
+++ tests/test_base.py
@@ -3,36 +3,34 @@ from flask import current_app, url_for
 
 from main import app
 
+
 class MainTest(TestCase):
     def create_app(self):
-        app.config["TESTING"] = True
-        app.config["WTF CSRF ENABLED"] = False
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
 
         return app
 
-    def test_app_exits(self):
+    def test_app_exists(self):
         self.assertIsNotNone(current_app)
 
-
-    def test_app_in_test_modee(self):
-        self.assertTrue(current_app.config["TESTING"])
+    def test_app_in_test_mode(self):
+        self.assertTrue(current_app.config['TESTING'])
 
     def test_index_redirects(self):
         response = self.client.get(url_for('index'))
+
         self.assertRedirects(response, url_for('hello'))
 
     def test_hello_get(self):
         response = self.client.get(url_for('hello'))
+
         self.assert200(response)
 
     def test_hello_post(self):
-        fake_form = {
-            'username': 'fake',
-            'password': 'fake-password'
-        }
-        response = self.client.post(url_for('hello'), data=fake_form)
+        response = self.client.post(url_for('hello'))
 
-        self.assertRedirects(response, url_for('index'))
+        self.assertTrue(response.status_code, 405)
 
     def test_auth_blueprint_exists(self):
         self.assertIn('auth', self.app.blueprints)
@@ -46,3 +44,12 @@ class MainTest(TestCase):
         self.client.get(url_for('auth.login'))
 
         self.assertTemplateUsed('login.html')
+
+    def test_auth_login_post(self):
+        fake_form = {
+            'username': 'fake',
+            'password': 'fake-password'
+        }
+
+        response = self.client.post(url_for('auth.login'), data=fake_form)
+        self.assertRedirects(response, url_for('index'))
