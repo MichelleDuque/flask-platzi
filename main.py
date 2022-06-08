cc7aaacb62de8e6895557f3b9c26fafde527b7af
MichelleDuque <michelled_1994@hotmail.com>
2022-06-07 20:43:50 -0500

    se agrego form de log in

--- main.py
+++ main.py
@@ -7,6 +7,10 @@
 from multiprocessing import context
 from flask import Flask, request, make_response, redirect, render_template, session
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app) 
@@ -14,6 +18,11 @@ bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = 'SUPER SECRETO'
 
 todos = ['Amar a Liverpool', 'Mandarle un mensaje a Robertson que lo amo', 'Escribirle a Mohamed Salah que renueve']
+
+class LoginForm(FlaskForm):
+    username = StringField("Nombre de usuario", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Enviar")
 
 @app.errorhandler(404)
 def not_found(error):
@@ -36,9 +45,11 @@ def index():
 @app.route('/hello')
 def hello():
     user_ip = session.get('user_ip')
+    login_form = LoginForm()
     context = {
         'user_ip': user_ip,
         'todos': todos,
+        'login_form': login_form
     }
 
     return render_template('hello.html', **context)
