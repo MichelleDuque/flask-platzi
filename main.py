c4ba48ec0ac3076c4f5b26292021f744e5032ea2
MichelleDuque <michelled_1994@hotmail.com>
2022-06-07 19:47:53 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,44 @@
+# .\venv\Scripts\activate
+# $env:FLASK_APP="main.py"
+# $env:FLASK_DEBUG=1
+# $env:FLASK_ENV="development"
+# flask run
+
+from multiprocessing import context
+from flask import Flask, request, make_response, redirect, render_template, session
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app) 
+
+app.config['SECRET_KEY'] = 'SUPER SECRETO'
+
+todos = ['Amar a Liverpool', 'Mandarle un mensaje a Robertson que lo amo', 'Escribirle a Mohamed Salah que renueve']
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error= error)
+
+@app.errorhandler(500)
+def server_error(error):
+    return render_template('500.html', error= error)
+
+@app.route('/')
+def index():
+    user_ip = request.remote_addr
+
+    response = make_response(redirect('/hello'))
+    session['user_ip'] = user_ip
+
+    return response
+
+
+@app.route('/hello')
+def hello():
+    user_ip = session.get('user_ip')
+    context = {
+        'user_ip': user_ip,
+        'todos': todos,
+    }
+
+    return render_template('hello.html', **context)
